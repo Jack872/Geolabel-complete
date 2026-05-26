dd1a7d8031c5ffa4a0f0d754240014e919a05542
fkdxhc <fkdxhc@gmail.com>
2026-05-26 15:59:31 +0800

    1、修复“审核报告”时间展示BUG

--- Backend(SpringBoot)/src/main/java/com/example/labelMark/domain/AuditInfo.java
+++ Backend(SpringBoot)/src/main/java/com/example/labelMark/domain/AuditInfo.java
@@ -4,6 +4,7 @@ import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
+import com.fasterxml.jackson.annotation.JsonFormat;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
@@ -110,7 +111,8 @@ public class AuditInfo implements Serializable {
     @TableField("auditor")
     private String auditor;
 
-    @ApiModelProperty("审核员")
+    @ApiModelProperty("审核完成时间")
+    @JsonFormat(pattern = "yyyy-MM-dd")
     @TableField("audit_time")
     private LocalDate auditTime;
 }
